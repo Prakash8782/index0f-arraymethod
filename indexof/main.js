0df6672a27ca4140bab4a00fa612cb2be32ada3a
Prakash8782 <prakash8782@gmail.com>
2022-09-19 14:51:30 +0530

    indexof

---
+++ indexof/main.js
@@ -0,0 +1,7 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+console.log("colors : " , colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.indexOf()");
+let a=colors.indexOf("Red");
+console.log("change in array : ",a);
+console.log("return : length of array after indexOf(); is "+colors.length);
